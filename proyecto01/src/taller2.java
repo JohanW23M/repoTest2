025bab549ef3c1d6553098f88a5e3b0d47677418
JohanW23M <jawang@utpl.edu.ec>
2024-10-17 20:05:54 -0500

    segundo commit

--- proyecto01/src/taller2.java
+++ proyecto01/src/taller2.java
@@ -1,7 +1,7 @@
 
 public class taller2 {
     public static void main(String[] args){
-      System.out.println ("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub");
+      System.out.println ("Ejemplo básico de Java Versionado a través de Git Manejado desde GitHub Creado por <JohanW23M");
     }
 
 }
